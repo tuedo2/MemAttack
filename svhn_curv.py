c91ddecf322fb0d0fe4a512bf918bb6d8f62233b
tuedo2 <47128027+tuedo2@users.noreply.github.com>
2025-09-16 12:26:34 -0500

    Update svhn_curv.py

--- svhn_curv.py
+++ svhn_curv.py
@@ -8,8 +8,6 @@ from utils import full_train_VGG11, full_train_resnet, full_train_mobilenet
 from attacks import SubsetTransformDataset, ReplaceWithDataset, Deepfool, Pseudoinverse, NaiveMaxEMD
 from scoring import get_curv_scores_for_net
 
-from mat import full_train_VGG11_mat, full_train_resnet_mat
-
 svhn = torchvision.datasets.SVHN(root='./data', split='train', download=True)
 
 default_transform = transforms.Compose([
@@ -50,33 +48,6 @@ def replace_attack(dir_name, replace_dataset, net_type='VGG', resize=(32, 32)):
             scores = get_curv_scores_for_net(new_dset, net)
             score_dict = dict(subset=subset_idx, scores=scores)
             np.savez(f'{dir_path}/run_{i+1}', **score_dict)
-
-def replace_mat_attack(dir_name, replace_dataset, net_type='Resnet', resize=(32, 32)):
-    for size in sizes:
-        dir_path = f'{BASE_DIR}/{dir_name}_{size}'
-        try:
-            os.mkdir(dir_path) # make directory to keep scores if not already created
-        except:
-            continue
-
-        for i in range(num_runs):
-            print(f'Saving scores at {dir_name} for size {size} run {i+1}...')
-            subset_idx = torch.randperm(len(svhn))[:size]
-            new_dset = SubsetTransformDataset(svhn, subset_idx, 
-                                              transforms.Compose([
-                                                transforms.ToTensor(), 
-                                                ReplaceWithDataset(replace_dataset, resize),
-                                                transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))]), 
-                                                default_transform)
-            if net_type == 'VGG':
-                net = full_train_VGG11_mat(new_dset)
-            if net_type == 'Resnet':
-                net = full_train_resnet_mat(new_dset)
-            # elif net_type == 'Mobile':
-            #     net = full_train_mobilenet(new_dset)            
-            scores = get_curv_scores_for_net(new_dset, net)
-            score_dict = dict(subset=subset_idx, scores=scores)
-            np.savez(f'{dir_path}/run_{i+1}', **score_dict) 
 
 
 def deepfool_attack(dir_name, overshoot=0.02, net_type='VGG'):
@@ -139,33 +110,6 @@ def pinv_attack(dir_name, net_type='VGG'):
             scores = get_curv_scores_for_net(new_dset, net)
             score_dict = dict(subset=subset_idx, scores=scores)
             np.savez(f'{dir_path}/run_{i+1}', **score_dict)
-
-def pinv_mat_attack(dir_name, net_type='Resnet'):
-    for size in sizes:
-        dir_path = f'{BASE_DIR}/{dir_name}_{size}'
-        try:
-            os.mkdir(dir_path) # make directory to keep scores if not already created
-        except:
-            continue
-
-        for i in range(num_runs):
-            print(f'Saving scores at {dir_name} for size {size} run {i+1}...')
-            subset_idx = torch.randperm(len(svhn))[:size]
-            new_dset = SubsetTransformDataset(svhn, subset_idx, 
-                                              transforms.Compose([
-                                                transforms.ToTensor(),
-                                                transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970)),   
-                                                Pseudoinverse()]),
-                                                default_transform)
-            if net_type == 'VGG':
-                net = full_train_VGG11_mat(new_dset)
-            if net_type == 'Resnet':
-                net = full_train_resnet_mat(new_dset)
-            # elif net_type == 'Mobile':
-            #     net = full_train_mobilenet(new_dset)
-            scores = get_curv_scores_for_net(new_dset, net)
-            score_dict = dict(subset=subset_idx, scores=scores)
-            np.savez(f'{dir_path}/run_{i+1}', **score_dict) 
 
 
 def naive_emd_attack(dir_name, net_type='VGG'):
